0cd99d6068280c4c693597d10afa517345643eaa
Jaemi Graham <emailme@jaemigraham.com>
2017-02-13 11:10:02 -0500

    Checkpoint 8 complete; Assignment 8 complete

---
+++ scripts/landing.js
@@ -0,0 +1,15 @@
+var animatePoints = function () {
+
+   var points = document.getElementsByClassName('point');
+
+   var revealPoint = function (a) {
+      points[a].style.opacity = 1;
+      points[a].style.transform = "scaleX(1) translateY(0)";
+      points[a].style.msTransform = "scaleX(1) translateY(0)";
+      points[a].style.WebkitTransform = "scaleX(1) translateY(0)";
+   }
+
+   for (var i = 0; i < points.length; i++) {
+      revealPoint(i);
+   }
+};
