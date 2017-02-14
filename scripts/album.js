3fb70067504f3517ea6ddcf47d4555c20e21b4cb
Jaemi Graham <emailme@jaemigraham.com>
2017-02-14 12:15:55 -0500

    Assignment 11 complete

---
+++ scripts/album.js
@@ -0,0 +1,137 @@
+// Example Album
+var albumPicasso = {
+   title: 'The Colors',
+   artist: 'Pablo Picasso',
+   label: 'Cubism',
+   year: '1881',
+   albumArtUrl: 'assets/images/album_covers/01.png',
+   songs: [
+      {
+         title: 'Blue',
+         duration: '4:26'
+      },
+      {
+         title: 'Green',
+         duration: '3:14'
+      },
+      {
+         title: 'Red',
+         duration: '5:01'
+      },
+      {
+         title: 'Pink',
+         duration: '3:21'
+      },
+      {
+         title: 'Magenta',
+         duration: '2:15'
+      }
+   ]
+};
+
+// Another Example Album
+var albumMarconi = {
+   title: 'The Telephone',
+   artist: 'Guglielmo Marconi',
+   label: 'EM',
+   year: '1909',
+   albumArtUrl: 'assets/images/album_covers/20.png',
+   songs: [
+      {
+         title: 'Hello, Operator?',
+         duration: '1:01'
+      },
+      {
+         title: 'Ring, ring, ring',
+         duration: '5:01'
+      },
+      {
+         title: 'Fits in your pocket',
+         duration: '3:21'
+      },
+      {
+         title: 'Can you hear me now?',
+         duration: '3:14'
+      },
+      {
+         title: 'Wrong phone number',
+         duration: '2:15'
+      }
+   ]
+};
+
+// Third Album
+var albumFire = {
+   title: 'Fire',
+   artist: 'So Hot',
+   label: 'H.O.T.',
+   year: '2017',
+   albumArtUrl: 'assets/images/album_covers/08.png',
+   songs: [
+      {
+         title: 'Call 911!',
+         duration: '9:11'
+      },
+      {
+         title: 'Fireman ah Come',
+         duration: '5:01'
+      },
+      {
+         title: 'The Heat is On',
+         duration: '3:35'
+      },
+      {
+         title: 'Water Will Burn',
+         duration: '3:14'
+      },
+      {
+         title: 'Inferno',
+         duration: '5:14'
+      },
+      {
+         title: 'Too Hot to Handle',
+         duration: '3:15'
+      }
+   ]
+};
+
+var createSongRow = function (songNumber, songName, songLength) {
+   var template =
+      '<tr class="album-view-song-item">' + '  <td class="song-item-number">' + songNumber + '</td>' + '  <td class="song-item-title">' + songName + '</td>' + '  <td class="song-item-duration">' + songLength + '</td>' + '</tr>';
+
+   return template;
+};
+
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+var setCurrentAlbum = function (album) {
+
+   albumTitle.firstChild.nodeValue = album.title;
+   albumArtist.firstChild.nodeValue = album.artist;
+   albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+   albumImage.setAttribute('src', album.albumArtUrl);
+
+   albumSongList.innerHTML = '';
+
+   for (var i = 0; i < album.songs.length; i++) {
+      albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+   }
+};
+
+window.onload = function () {
+   setCurrentAlbum(albumPicasso);
+
+   var albums = [albumPicasso, albumMarconi, albumFire];
+   var index = 1;
+   albumImage.addEventListener("click", function (event) {
+      setCurrentAlbum(albums[index]);
+      index++;
+      if (index == albums.length) {
+         index = 0;
+      }
+   });
+};
