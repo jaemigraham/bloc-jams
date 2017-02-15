b6474bc9d8d3d9321feb00b904f660239b40a3d1
Jaemi Graham <emailme@jaemigraham.com>
2017-02-15 09:23:27 -0500

    Checkpoint 12 complete

--- scripts/album.js
+++ scripts/album.js
@@ -97,7 +97,7 @@ var albumFire = {
 
 var createSongRow = function (songNumber, songName, songLength) {
    var template =
-      '<tr class="album-view-song-item">' + '  <td class="song-item-number">' + songNumber + '</td>' + '  <td class="song-item-title">' + songName + '</td>' + '  <td class="song-item-duration">' + songLength + '</td>' + '</tr>';
+      '<tr class="album-view-song-item">' + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>' + '  <td class="song-item-title">' + songName + '</td>' + '  <td class="song-item-duration">' + songLength + '</td>' + '</tr>';
 
    return template;
 };
@@ -122,16 +122,34 @@ var setCurrentAlbum = function (album) {
    }
 };
 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
 window.onload = function () {
    setCurrentAlbum(albumPicasso);
 
-   var albums = [albumPicasso, albumMarconi, albumFire];
-   var index = 1;
-   albumImage.addEventListener("click", function (event) {
-      setCurrentAlbum(albums[index]);
-      index++;
-      if (index == albums.length) {
-         index = 0;
+   songListContainer.addEventListener('mouseover', function (event) {
+      if (event.target.parentElement.className === 'album-view-song-item') {
+         event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
       }
    });
-};
+
+   for (var i = 0; i < songRows.length; i++) {
+      songRows[i].addEventListener('mouseleave', function (event) {
+         // Selects first child element, which is the song-item-number element
+         this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+      });
+   }
+}
+
+var albums = [albumPicasso, albumMarconi, albumFire];
+var index = 1;
+albumImage.addEventListener("click", function (event) {
+   setCurrentAlbum(albums[index]);
+   index++;
+   if (index == albums.length) {
+      index = 0;
+   }
+});
